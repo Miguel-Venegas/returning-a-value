5b24859759737b99311554bd5d72bf10961920c1
Miguel-Venegas <venegas.a.miguel@gmail.com>
2020-12-20 08:07:38 -0800

    difference between console.log and return

---
+++ app.js
@@ -0,0 +1,14 @@
+//Learning about the difference between return and console.log
+
+// No return!
+function add(x, y) {
+	console.log(x + y);
+}
+
+// This version returns the sum of x & y;
+function add(x, y) {
+	return x + y;
+}
+
+// We can capture the return value:
+const total = add(4, 9); //13
